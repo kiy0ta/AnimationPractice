2026581c9c8bdbce114654e2a0538dcd94d9e385
kiyo <i.mitsuki19@gmail.com>
2021-05-08 18:03:02 +0900

    - update glide to 4.x  - add gif animation

--- app/src/main/java/com/example/animationpractice/MainActivity.kt
+++ app/src/main/java/com/example/animationpractice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.animationpractice
 
 import android.animation.Animator
 import android.animation.Animator.AnimatorListener
+import android.graphics.drawable.Drawable
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
@@ -12,7 +13,14 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import com.airbnb.lottie.LottieAnimationView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var goodButtonImageView: ImageView
@@ -64,6 +72,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun showGifAnimation() {
+        imageView.visibility = VISIBLE
+        // glideを使用しない場合
+//        val source = ImageDecoder.createSource(assets, "splash_gif.gif")
+//        val gifAnimationDrawable = ImageDecoder.decodeDrawable(source) as? AnimatedImageDrawable
+//            ?: throw ClassCastException()
+//        imageView.setImageDrawable(gifAnimationDrawable)
+//        gifAnimationDrawable.start()
+        // glide(4.x)を使用する場合(3.xと4.xでは書き方が異なるので注意する)
+        Glide.with(this)
+            .asGif()
+            .load(R.raw.splash_gif)
+            .listener(object : RequestListener<GifDrawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<GifDrawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: GifDrawable?,
+                    model: Any?,
+                    target: Target<GifDrawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    resource!!.setLoopCount(1)
+                    resource.registerAnimationCallback(object :
+                        Animatable2Compat.AnimationCallback() {
+                        override fun onAnimationEnd(drawable: Drawable?) {
+                            super.onAnimationEnd(drawable)
+                            imageView.visibility = GONE
+                        }
+                    }
+                    )
+                    return false
+                }
+            })
+            .into(imageView)
     }
 
     private fun showJsonAnimation() {
